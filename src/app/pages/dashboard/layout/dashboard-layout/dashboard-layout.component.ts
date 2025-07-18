b934bf72a5b255d96b20367e1e77e8466dec951b
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-07-18 08:45:35 -0300

    add footer

--- src/app/pages/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/pages/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent],
+  imports: [SidebarComponent, NavbarComponent, FooterComponent],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
