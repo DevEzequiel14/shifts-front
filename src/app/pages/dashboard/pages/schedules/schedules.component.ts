8b3dc793df49957e519cf8fb790ad1b1cf334d56
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-07-24 11:33:20 -0300

    change the buttons

--- src/app/pages/dashboard/pages/schedules/schedules.component.ts
+++ src/app/pages/dashboard/pages/schedules/schedules.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { GeneralConfigurationComponent } from './general-configuration/general-configuration.component';
 import { DaysSchedulesComponent } from './days-schedules/days-schedules.component';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-schedules',
   standalone: true,
-  imports: [GeneralConfigurationComponent, DaysSchedulesComponent],
+  imports: [GeneralConfigurationComponent, DaysSchedulesComponent, ButtonComponent],
   templateUrl: './schedules.component.html',
   styleUrl: './schedules.component.css'
 })
